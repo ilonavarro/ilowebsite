120bc1640ba9448298de5b2d900a8feaab183ed3
ilonavarro <ilo.navarro@gmail.com>
2022-08-05 12:41:27 +0100

    First Commit

---
+++ newpage/src/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <img src='https://github.com/ilonavarro.png' alt='Ilo Navarro' className='siteImage'/>
+        <h1 className='siteName'>Ilo Navarro</h1>
+    </div>
+  )
+}
+
+export default Header
